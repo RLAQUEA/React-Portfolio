8a2ce5167806823c8f0ecf6728424b79e17c308f
rlaquea <rlaquea14@gmail.com>
2021-08-23 15:28:49 -0400

    removed unnecessary code from home & nav components, reinstall npm & react-router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import from '../styles/Nav.css';
-//use style prop to
+// import React from 'react';
+// import from '../styles/Nav.css';
+// //use style prop to
 
-const styles = {
-    card: {
-margin: 
-background:
-},
-heading: {
-background
-minHeight:
-lineHeight: 
-fontSize: 
-color: 
-padding:
-}
-};
+// const styles = {
+//     card: {
+// margin: '',
+// background: '',
+// },
+// heading: {
+// background: '#9a74db',
+// minHeight: '',
+// lineHeight: '',
+// fontSize: '',
+// color: '',
+// padding: '',
+// }
+// };
+
+// function Nav() {
+//     return (
+//         <div style={styles.card} className="nav">
+//         <div style={styles.heading}>Home</div>
+//     )
+// }
