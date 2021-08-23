cc78c4b4069ddc9c12e86c93939109cf5e545556
rlaquea <rlaquea14@gmail.com>
2021-08-23 10:41:34 -0400

    setting up css file structure for Nav & header

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import from '../styles/Nav.css';
+//use style prop to
+
+const styles = {
+    card: {
+margin: 
+background:
+},
+heading: {
+background
+minHeight:
+lineHeight: 
+fontSize: 
+color: 
+padding:
+}
+};
